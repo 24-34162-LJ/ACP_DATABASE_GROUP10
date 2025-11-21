2f0efe18196699e3b51bd487917e3dea02d95f46
LANCE JHOMAR BRAGA <24-34162@g.batstate-u.edu.ph>
2025-11-21 22:23:49 +0800

    hehe

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, redirect, url_for, request
+from models import db
 
-app = Flask(__name__)
+app = Flask(__name__) # to connect the app.py to the models.py
+
 app.config['SECRET_KEY'] = 'lj123' # for security purposes
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db' # this create a database /save the data
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # this to remove unessary tracking
+
+db.init_app(app)
 
 @app.route('/home')
 def home():
@@ -20,20 +26,9 @@ def index():
   return redirect(url_for("home"))
 
 if __name__ == "__main__":
+  # this create first all the table in out database it is a content manager
+  with app.app_context():
+    db.create_all()
+
   app.run(debug=True)
   
-  
-  """
-  
-  for post and get use this for login
-  @app.route('/login', methods=['POST', 'GET'])
-  
-  from flask import request
-  - to know if we get get or post = request
-  
-  """
-  
-  """
-  app = responsible for routing 
-  
-  """
---
+++ models.py
@@ -0,0 +1,3 @@
+from flask_sqlalchemy import SQLAlchemy # this import the databse
+
+db = SQLAlchemy() # to get the database / schema
